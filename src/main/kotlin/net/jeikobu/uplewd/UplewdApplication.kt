031fe90abdbfd1dd0b6daa9da0494235c3dbbfb5
jeikobu__ <github@jeikobu.net>
2022-08-18 20:45:05 +0200

    Initial commit

---
+++ src/main/kotlin/net/jeikobu/uplewd/UplewdApplication.kt
@@ -0,0 +1,11 @@
+package net.jeikobu.uplewd
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class UplewdApplication
+
+fun main(args: Array<String>) {
+    runApplication<UplewdApplication>(*args)
+}
