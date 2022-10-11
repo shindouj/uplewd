d5f9ed53c27635475800e11c5ebe3def3a3d1fde
pw <68441479+amadeo-alex@users.noreply.github.com>
2022-10-11 21:21:34 +0200

    fixed poor man's UI; added error handling

--- src/main/resources/static/assets/js/uploader.js
+++ src/main/resources/static/assets/js/uploader.js
@@ -1,6 +1,6 @@
 if (!String.prototype.format) {
-    String.prototype.format = function(...args) {
-        return this.replace(/(\{\d+\})/g, function(a) {
+    String.prototype.format = function (...args) {
+        return this.replace(/(\{\d+\})/g, function (a) {
             return args[+(a.substr(1, a.length - 2)) || 0];
         });
     };
@@ -11,35 +11,14 @@ const uploadList = document.getElementById("uploadList");
 const fileSelection = document.getElementById("fileSelection");
 let fileList = [];
 
-const uploadStatusTemplate = `
-    <div class="card bg-light p-0 h-100 shadow-sm">
-        <div class="row h-100 g-0 rounded flex-nowrap">
-            <div class="col-auto bg-info rounded-start">&nbsp;</div>
-            <div class="col p-3 py-4 text-info">
-                <div class="d-flex flex-row">
-                    <h6 class="text-truncate text-uppercase">URL: </h6>
-                    <h6 class="text-truncate text-uppercase">
-                        <a href="//{0}">{0}</a>
-                    </h6>
-                </div>
-                <div class="d-flex flex-row">
-                    <h6 class="text-truncate text-uppercase">Deletion URL: </h6>
-                    <h6 class="text-truncate text-uppercase">
-                        <a href="//{1}">{1}</a>
-                    </h6>
-                </div>
-            </div>
-            <div class="col-auto d-flex rounded-end">
-                <h1 class="mb-0 mx-auto align-self-center">
-                    <a href="#" title="View details"
-                        class="nav-link text-info pb-0">
-                        <span class="d-inline-block lnr lnr-cart"></span>
-                        </a>
-                </h1>
-            </div>
-        </div>
-    </div>
-`;
+let uploadStatusTemplate;
+let uploadStatusTemplateRequest = new XMLHttpRequest();
+uploadStatusTemplateRequest.onload = function () {
+    uploadStatusTemplate = uploadStatusTemplateRequest.responseText;
+}
+uploadStatusTemplateRequest.open("GET", "/assets/html/uploadStatusTemplate.html");
+uploadStatusTemplateRequest.send();
+
 
 function uploadFile(file) {
     let formData = new FormData();
@@ -50,17 +29,56 @@ function uploadFile(file) {
 
     let request = new XMLHttpRequest();
     let url = window.location.origin + "/upload";
+
     request.onreadystatechange = function () {
-        if(request.readyState === XMLHttpRequest.DONE){
-            let response = JSON.parse(request.responseText);
+        const fileName = file.name;
+        if (request.readyState === XMLHttpRequest.OPENED) {
+            let uploadEntry = uploadStatusTemplate.format(fileName, "response.url", "response.deletionUrl");
 
-            //debugBox.innerHTML += request.responseText;
-            //debugBox.innerHTML += "<br>";
+            let uploadEntryContainer = document.createElement("div");
+            uploadEntryContainer.setAttribute("id", fileName)
+            uploadEntryContainer.setAttribute("class", "mb-2");
+            uploadEntryContainer.innerHTML = uploadEntry
+            uploadList.appendChild(uploadEntryContainer);
+        } else if (request.readyState === XMLHttpRequest.DONE) {
+            let uploadEntryContainer = document.getElementById(fileName);
+            let progressBar = uploadEntryContainer.getElementsByClassName("progress")[0]
+                .getElementsByClassName("progress-bar")[0];
+            if (request.status === 200) {
+                let response = JSON.parse(request.responseText);
 
-            let uploadEntry = uploadStatusTemplate.format(response.url, response.deletionUrl);
-            uploadList.innerHTML += uploadEntry
+                uploadEntryContainer.getElementsByClassName("fileUrl")[0].innerHTML = response.url;
+                uploadEntryContainer.getElementsByClassName("fileUrl")[0]
+                    .setAttribute("href", `//${response.url}`);
+                uploadEntryContainer.getElementsByClassName("fileDUrl")[0].innerHTML = response.deletionUrl;
+                uploadEntryContainer.getElementsByClassName("fileDUrl")[0]
+                    .setAttribute("href", `//${response.deletionUrl}`);
+
+                uploadEntryContainer.getElementsByClassName("fileLinks")[0]
+                    .setAttribute("style", "display: initial");
+
+                progressBar.setAttribute("class", "progress-bar bg-success");
+                progressBar.innerHTML = "Done!";
+            } else {
+                progressBar.setAttribute("class", "progress-bar bg-danger");
+                progressBar.innerHTML = `Error! (${request.status})`;
+
+                //alert(`error(${request.status}): ${request.responseText}`);
+            }
         }
     }
+    request.upload.addEventListener("progress", function (ev) {
+        const fileName = file.name;
+        let uploadEntryContainer = document.getElementById(fileName);
+        let percent = (ev.loaded / ev.total) * 100;
+        let progressBar = uploadEntryContainer.getElementsByClassName("progress")[0]
+            .getElementsByClassName("progress-bar")[0];
+        progressBar.innerHTML = `${percent}%`;
+        progressBar.setAttribute("style", `width: ${percent}%`);
+    });
+    request.addEventListener("error", function (ev) {
+        alert(`error: ${request.responseText}`);
+    })
 
     request.open("POST", url);
     request.setRequestHeader(header, token);
@@ -78,6 +96,7 @@ const fileForm = document.getElementById("fileForm")
 
 fileForm.addEventListener("submit", function (event) {
     event.preventDefault();
+    uploadList.replaceChildren();
     fileList.forEach(function (file) {
         uploadFile(file);
     })
