80e795ff0f678d3ee5794d0540ca02592d7d6234
pw <68441479+amadeo-alex@users.noreply.github.com>
2022-10-14 20:00:45 +0200

    slight code refactor

--- src/main/resources/static/assets/js/uploader.js
+++ src/main/resources/static/assets/js/uploader.js
@@ -1,7 +1,7 @@
 if (!String.prototype.format) {
     String.prototype.format = function (...args) {
-        return this.replace(/(\{\d+\})/g, function (a) {
-            return args[+(a.substr(1, a.length - 2)) || 0];
+        return this.replace(/{([0-9]+)}/g, function (match, index) {
+            return typeof args[index] == 'undefined' ? match : args[index];
         });
     };
 }
@@ -9,6 +9,8 @@ if (!String.prototype.format) {
 const debugBox = document.getElementById("debugBox");
 const uploadList = document.getElementById("uploadList");
 const fileSelection = document.getElementById("fileSelection");
+const fileForm = document.getElementById("fileForm");
+
 let fileList = [];
 
 let uploadStatusTemplate;
@@ -19,6 +21,20 @@ uploadStatusTemplateRequest.onload = function () {
 uploadStatusTemplateRequest.open("GET", "/assets/html/uploadStatusTemplate.html");
 uploadStatusTemplateRequest.send();
 
+fileSelection.addEventListener("change", function (event) {
+    fileList = [];
+    for (let i = 0; i < fileSelection.files.length; i++) {
+        fileList.push(fileSelection.files[i]);
+    }
+});
+
+fileForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+    uploadList.replaceChildren();
+    fileList.forEach(function (file) {
+        uploadFile(file);
+    })
+});
 
 function uploadFile(file) {
     let formData = new FormData();
@@ -31,28 +47,28 @@ function uploadFile(file) {
     let url = window.location.origin + "/upload";
 
     request.onreadystatechange = function () {
-        const fileName = file.name;
         if (request.readyState === XMLHttpRequest.OPENED) {
-            let uploadEntry = uploadStatusTemplate.format(fileName, "response.url", "response.deletionUrl");
-
+            let uploadEntry = uploadStatusTemplate.format(file.name, "response.url", "response.deletionUrl");
             let uploadEntryContainer = document.createElement("div");
-            uploadEntryContainer.setAttribute("id", fileName)
+            uploadEntryContainer.setAttribute("id", file.name);
             uploadEntryContainer.setAttribute("class", "mb-2");
-            uploadEntryContainer.innerHTML = uploadEntry
+            uploadEntryContainer.innerHTML = uploadEntry;
+
             uploadList.appendChild(uploadEntryContainer);
         } else if (request.readyState === XMLHttpRequest.DONE) {
-            let uploadEntryContainer = document.getElementById(fileName);
+            let uploadEntryContainer = document.getElementById(file.name);
             let progressBar = uploadEntryContainer.getElementsByClassName("progress")[0]
                 .getElementsByClassName("progress-bar")[0];
             if (request.status === 200) {
                 let response = JSON.parse(request.responseText);
 
-                uploadEntryContainer.getElementsByClassName("fileUrl")[0].innerHTML = response.url;
-                uploadEntryContainer.getElementsByClassName("fileUrl")[0]
-                    .setAttribute("href", `//${response.url}`);
-                uploadEntryContainer.getElementsByClassName("fileDUrl")[0].innerHTML = response.deletionUrl;
-                uploadEntryContainer.getElementsByClassName("fileDUrl")[0]
-                    .setAttribute("href", `//${response.deletionUrl}`);
+                let fileUrl = uploadEntryContainer.getElementsByClassName("fileUrl")[0];
+                fileUrl.innerHTML = response.url;
+                fileUrl.setAttribute("href", `//${response.url}`);
+
+                let fileDUrl = uploadEntryContainer.getElementsByClassName("fileDUrl")[0];
+                fileDUrl.innerHTML = response.deletionUrl;
+                fileDUrl.setAttribute("href", `//${response.deletionUrl}`);
 
                 uploadEntryContainer.getElementsByClassName("fileLinks")[0]
                     .setAttribute("style", "display: initial");
@@ -62,44 +78,26 @@ function uploadFile(file) {
             } else {
                 progressBar.setAttribute("class", "progress-bar bg-danger");
                 progressBar.innerHTML = `Error! (${request.status})`;
-
-                //alert(`error(${request.status}): ${request.responseText}`);
             }
         }
-    }
+    };
+
     request.upload.addEventListener("progress", function (ev) {
-        const fileName = file.name;
-        let uploadEntryContainer = document.getElementById(fileName);
+        let uploadEntryContainer = document.getElementById(file.name);
         let percent = (ev.loaded / ev.total) * 100;
         let progressBar = uploadEntryContainer.getElementsByClassName("progress")[0]
             .getElementsByClassName("progress-bar")[0];
         progressBar.innerHTML = `${percent}%`;
         progressBar.setAttribute("style", `width: ${percent}%`);
     });
+
     request.addEventListener("error", function (ev) {
         alert(`error: ${request.responseText}`);
-    })
+    });
 
     request.open("POST", url);
     request.setRequestHeader(header, token);
     request.send(formData);
 }
-
-fileSelection.addEventListener("change", function (event) {
-    fileList = [];
-    for (let i = 0; i < fileSelection.files.length; i++) {
-        fileList.push(fileSelection.files[i]);
-    }
-})
-
-const fileForm = document.getElementById("fileForm")
-
-fileForm.addEventListener("submit", function (event) {
-    event.preventDefault();
-    uploadList.replaceChildren();
-    fileList.forEach(function (file) {
-        uploadFile(file);
-    })
-})
 
 debugBox.innerHTML += "<br>Attached<br>";
