980ba5a31f05f86f5470b41c74c33d533feb438b
pw <68441479+amadeo-alex@users.noreply.github.com>
2022-11-24 22:26:30 +0100

    added rudimentary search box to user profile

--- src/main/resources/static/assets/js/userPanel.js
+++ src/main/resources/static/assets/js/userPanel.js
@@ -2,6 +2,9 @@ const refreshTokenButton = document.getElementById("refreshTokenButton");
 const tokenParagraph = document.getElementById("userTokenParagraph");
 const copyTokenButton = document.getElementById("copyTokenButton");
 
+const searchQuery = document.getElementById("searchQuery");
+const searchButton = document.getElementById("searchButton");
+
 refreshTokenButton.addEventListener("click", function () {
     let token = $("meta[name='_csrf']").attr("content");
     let header = $("meta[name='_csrf_header']").attr("content");
@@ -27,3 +30,16 @@ refreshTokenButton.addEventListener("click", function () {
 copyTokenButton.addEventListener("click", function () {
     navigator.clipboard.writeText(tokenParagraph.innerText);
 })
+
+searchButton.addEventListener("click", function () {
+    let url = new URL(window.location);
+    url.searchParams.set("searchQuery", searchQuery.value);
+    window.location = url;
+})
+
+window.onload = function () {
+    let url = new URL(window.location);
+    if(url.searchParams.has("searchQuery")){
+        searchQuery.value = url.searchParams.get("searchQuery");
+    }
+}
